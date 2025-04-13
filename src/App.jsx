a4761ee600cbd2d114e275ab59cf0e21c08bea8e
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-04-14 01:25:15 +0600

    resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 
 import "./App.css";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello World</h1>
+      <navbar></navbar>
     </>
   );
 }
